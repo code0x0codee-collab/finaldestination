d78fe145f2a80da8a809b0d04e09b48ef7c3c293
code0x0codee-collab <code0x0codee@gmail.com>
2026-09-13 07:02:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blogging_Application
+ express base Blogging application
--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-import {connectDB} from "./Database/index.js";
-import dotenv from "dotenv";
 import express from "express"
-import {app as ap} from "./app.js"
-
-
-dotenv.config({
-    path:'./.env'
+const app=express();
+import path from "path"
+//configure dotenv
+import dotenv from "dotenv"
+dotenv.config()
+//ejs files setup
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+app.get("/",(req,res)=>{
+    res.render("homepage")
 })
 
-const app=express()
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT|| 8001 , ()=>{
-        console.log(`Server is running on Port: ${process.env.PORT}`)
-    })
-    
-    app.on("error",(error)=>{
-        console.log("ERROR: ",error)
-        throw error
-    })
-})
-.catch((err)=>{
-    console.log(`Mysql error in index.js of src ${err}`)
-})
-
-app.use(ap)
+app.listen(process.env.PORT)
